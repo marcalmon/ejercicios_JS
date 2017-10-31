b2b294bc4c7db5fac462d41ae7473282bd92863c
Mar Calmon <marical2222@gmail.com>
2017-10-31 20:29:58 -0300

    batalla naval y objetos

---
+++ objetos_JS/push_pop.js
@@ -0,0 +1,29 @@
+//var disfraces = new Array();//creo un nuevo objeto de clase Array
+
+//disfraces.push("Unicornio");
+
+var notas = ["do","re", "mi", "fa","sol"];
+
+//Métodos push  y pop
+var notasPush = notas.push("la", "si");//retorna la longitud del array
+
+console.log(notasPush);//6
+console.log("¿El valor que retorna push es igual al length del array?: ", notasPush == notas.length);//true
+
+console.log("Array con push: " + notas);
+
+/*notas.pop();
+console.log(notas);
+
+var notaEliminada = notas.pop();
+console.log(notaEliminada);*/
+
+var notaSi = notas.pop();
+var notaLa = notas.pop();
+console.log(notas);
+console.log(notaSi, notaLa);
+
+console.log(notas.indexOf("fa"));
+
+
+
